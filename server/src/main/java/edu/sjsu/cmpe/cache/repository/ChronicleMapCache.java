4e72d5345bedce8bab45561aa48d1f6279cc2cc0
YashaswiThannir <yashaswi.t93@gmail.com>
2015-05-16 22:39:52 -0700

    lab3
    lab3

--- server/src/main/java/edu/sjsu/cmpe/cache/repository/ChronicleMapCache.java
+++ server/src/main/java/edu/sjsu/cmpe/cache/repository/ChronicleMapCache.java
@@ -18,7 +18,6 @@ public class ChronicleMapCache implements CacheInterface {
 	private ChronicleMap<Long, Entry> chronicleMap;
 	
 	public ChronicleMapCache(String fileName) throws IOException {
-		ChronicleMapBuilder<Long, Entry> builder=ChronicleMapBuilder.of(Long.class, Entry.class).constantValueSizeBySample(new Entry());
 		chronicleMap=builder.createPersistedTo(new File(fileName));
 	}
 	@Override
